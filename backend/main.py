4e8cb2e340ac70e9017e2a4542f50d897886f457
AmanThappa15 <amanthappa242@gmail.com>
2026-08-26 15:49:19 +0530

    implemented PDF upload and storage

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,13 @@
-from fastapi import FastAPI,UploadFile,File
+from fastapi import FastAPI, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
+import os
+import shutil
+
 app = FastAPI()
+
+UPLOAD_DIR = "uploads"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173"],
@@ -12,12 +19,20 @@ app.add_middleware(
 @app.get("/")
 def home():
     return {"message": "AI Study Assistant Backend is Working!"}
+
 @app.get("/health")
 def health_check():
-    return{"status": "Backend is healthy"}
+    return {"status": "Backend is healthy"}
+
 @app.post("/upload")
 async def upload_file(file: UploadFile = File(...)):
-    return{
-        "filename":file.filename,
-        "content_type" :file.content_type
+    file_path = os.path.join(UPLOAD_DIR, file.filename)
+
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    return {
+        "filename": file.filename,
+        "content_type": file.content_type,
+        "message": "File uploaded successfully"
     }
